ec350487e64844676bbb47241d97c50ca9bffc9c
MikeTaran <mtaran@ukr.net>
2023-07-17 01:46:19 +0300

    feat: asserts login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -7,4 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    # page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser,
+                           browser.current_url)  # Инициализируем LoginPage в теле теста
+    login_page.should_be_login_page()
